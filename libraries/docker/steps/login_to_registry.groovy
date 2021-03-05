a9750fec5cf0fb0a8cf964c6f5cf02aeed442f13
namcaoxuan <caoxuannam.cxn@gmail.com>
2021-03-05 18:27:35 +0700

    Update docker login with gcr

--- libraries/docker/steps/login_to_registry.groovy
+++ libraries/docker/steps/login_to_registry.groovy
@@ -7,7 +7,7 @@ void call(String _url = null, String _credentialId = null, def body){
 
   String protocol = config.registry_protocol ?: "https://"
   String url = _url ?: "${protocol}${repository}"
-  String credentialId = _credentialId ?: "gcr:[${cred}]"
+  String credentialId = _credentialId ?: "gcr:${cred}"
 
   docker.withRegistry(url, credentialId, body)
 
