c7bbc12710b218ea9288b8928136a6df24e190a0
namcaoxuan <caoxuannam.cxn@gmail.com>
2021-03-05 15:15:08 +0700

    Libraries

---
+++ libraries/docker/steps/login_to_registry.groovy
@@ -0,0 +1,14 @@
+
+package libraries.docker
+
+void call(String _url = null, String _credentialId = null, def body){
+
+  def (repository, cred) = get_registry_info()
+
+  String protocol = config.registry_protocol ?: "https://"
+  String url = _url ?: "${protocol}${repository}"
+  String credentialId = _credentialId ?: cred
+
+  docker.withRegistry(url, credentialId, body)
+
+}
---
+++ libraries/utility/steps/agent.groovy
@@ -0,0 +1,8 @@
+package libraries.utility 
+
+void call(Closure body) {
+  podTemplate(containers: [
+    containerTemplate(name: 'docker-dind-gcloud', image: 'axieinfinity/axie-docker-dind-gcloud:latest', ttyEnabled: true, command: 'cat'),
+  ]) {
+    node(POD_LABEL, body)
+}
