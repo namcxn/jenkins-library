9fb68071bb3c96faf62f05ee4ad78264dc4bf783
namcaoxuan <caoxuannam.cxn@gmail.com>
2021-03-07 01:02:19 +0700

    Update agent

--- libraries/utility/steps/agent.groovy
+++ libraries/utility/steps/agent.groovy
@@ -3,10 +3,12 @@ package libraries.utility
 void call(Closure body) {
   podTemplate(cloud: 'kubernetes', containers: [
     containerTemplate(name: 'docker-dind-gcloud', image: 'axieinfinity/axie-docker-dind-gcloud:latest', ttyEnabled: true, privileged: true, command: 'cat'),
+    containerTemplate(name: 'helm-gcloud', image: 'axieinfinity/axie-helm3-gcloud:latest', ttyEnabled: true, privileged: true, command: 'cat'),
   ],
   volumes: [
     emptyDirVolume(mountPath: "/var/lib/docker", memory: false),
-    hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')
+    hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock'),
+    configMapVolume(mountPath: '/root/.gcloud/', configMapName: 'helm-secrets-confimap'),
   ]) {
     node(POD_LABEL, body)
   }
