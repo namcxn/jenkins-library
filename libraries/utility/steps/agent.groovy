fac8734a12ccfbdbd97f15a1e93cf99c74036546
namcaoxuan <caoxuannam.cxn@gmail.com>
2021-03-07 00:10:09 +0700

    Update agent

--- libraries/utility/steps/agent.groovy
+++ libraries/utility/steps/agent.groovy
@@ -2,7 +2,7 @@ package libraries.utility
 
 void call(Closure body) {
   podTemplate(containers: [
-    containerTemplate(name: 'docker-dind-gcloud', image: 'axieinfinity/axie-docker-dind-gcloud:latest', ttyEnabled: true, command: 'cat'),
+    containerTemplate(name: 'docker-dind-gcloud', image: 'axieinfinity/axie-docker-dind-gcloud:latest', ttyEnabled: true, privileged: true, command: 'cat'),
   ]) {
     node(POD_LABEL, body)
   }
