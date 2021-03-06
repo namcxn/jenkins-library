98bf5b31f0bd7de4982f21335416ff60cf7e502e
namcaoxuan <caoxuannam.cxn@gmail.com>
2021-03-07 00:27:34 +0700

    Update agent

--- libraries/utility/steps/agent.groovy
+++ libraries/utility/steps/agent.groovy
@@ -5,7 +5,8 @@ void call(Closure body) {
     containerTemplate(name: 'docker-dind-gcloud', image: 'axieinfinity/axie-docker-dind-gcloud:latest', ttyEnabled: true, privileged: true, command: 'cat'),
   ],
   volumes: [
-    emptyDirVolume(mountPath: "/var/lib/docker")
+    emptyDirVolume(mountPath: "/var/lib/docker", memory: false),
+    hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')
   ]) {
     node(POD_LABEL, body)
   }
