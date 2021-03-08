2cd2e5c5adf41f23327e6814e94b6d7d50926fd6
namcaoxuan <caoxuannam.cxn@gmail.com>
2021-03-08 21:11:32 +0700

    Fix syntax

--- libraries/docker/steps/login_to_registry.groovy
+++ libraries/docker/steps/login_to_registry.groovy
@@ -7,10 +7,10 @@ void call(String _url = null, String _credentialId = null, def body){
 
   String protocol = config.registry_protocol ?: "https://"
   String url = _url ?: "${protocol}${repository}"
-  if ( repository =~ "(gcr.io|asia.gcr.io|eu.gcr.io|us.gcr.io)")
+  if ( repository =~ "(gcr.io|asia.gcr.io|eu.gcr.io|us.gcr.io)") {
     String credentialId = _credentialId ?: "gcr:${cred}"
     docker.withRegistry(url, credentialId, body)
-  else {
+  } else {
     String credentialId = _credentialId ?: cred
     docker.withRegistry(url, credentialId, body)
   }
