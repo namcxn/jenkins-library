44794077ab2cdcc1fc03531cc388bb213609c488
namcaoxuan <caoxuannam.cxn@gmail.com>
2021-03-08 21:27:21 +0700

    test with dockerhub

--- libraries/docker/steps/login_to_registry.groovy
+++ libraries/docker/steps/login_to_registry.groovy
@@ -6,7 +6,10 @@ void call(String _url = null, String _credentialId = null, def body){
   def (repository, cred) = get_registry_info()
 
   String protocol = config.registry_protocol ?: "https://"
-  String url = _url ?: "${protocol}${repository}"
+  if ( repository == ' ' ) {
+    String url = _url ?: ''
+  }
+  
   if ( repository =~ "(gcr.io|asia.gcr.io|eu.gcr.io|us.gcr.io)") {
     String credentialId = _credentialId ?: "gcr:${cred}"
     docker.withRegistry(url, credentialId, body)
