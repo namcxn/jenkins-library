da845d3a92492f1fc68618928a55f17cf0fc2c4b
namcaoxuan <caoxuannam.cxn@gmail.com>
2021-03-10 18:02:16 +0700

    Test get image to build

--- libraries/docker/steps/get_images_to_build.groovy
+++ libraries/docker/steps/get_images_to_build.groovy
@@ -46,7 +46,7 @@ def call(){
           //String service_name = service.split(".").first()
           // debug
           //echo "Print ${service_name}"
-          String service_name = (service == "Dockerfile") ? 'service' : service
+          String service_name = (service == "Dockerfile") ? 'svc' : service
             images.push([
               registry: image_reg,
               repo: "${path_prefix}${env.REPO_NAME}_${service_name}".toLowerCase(),
