0bf29180da9fdcbf82dc15ec0ae2f4834cf7332d
namcaoxuan <caoxuannam.cxn@gmail.com>
2021-03-14 01:34:42 +0700

    Rename func

--- vars/parameters.groovy
+++ vars/parameters.groovy
@@ -1,4 +1,4 @@
-public inputParameters() {
+public inputParametersTest() {
     def parameters = [
             string(name: 'aws_region', defaultValue: "us-east-1"),
             choice(name: 'environment', choices: "dev\nqa\nprod")
