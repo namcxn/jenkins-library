d7a5afdb500f6dcbeaf1b281e7b5ce7f45533ca0
namcaoxuan <caoxuannam.cxn@gmail.com>
2021-03-14 01:33:08 +0700

    Add lib input paramaters

---
+++ vars/parameters.groovy
@@ -0,0 +1,9 @@
+public inputParameters() {
+    def parameters = [
+            string(name: 'aws_region', defaultValue: "us-east-1"),
+            choice(name: 'environment', choices: "dev\nqa\nprod")
+    ]
+    return parameters
+}
+
+return this;
